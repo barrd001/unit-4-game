1bc960bea4b205f4ec9af53817c0a45a015e79b0
Devyn Barron <52061626+barrd001@users.noreply.github.com>
2019-08-16 22:22:37 -0700

    HTML and CSS set

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+var wins = 0;
+var losses = 0;
+var totalScore = 0
+
+function numberToGuess(max, min) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+function redValue() {
+    return Math.floor((Math.random() * 12 + 1));
+};
+
+function greenValue() {
+    return Math.floor((Math.random() * 12 + 1));
+}
+
+function orangeValue() {
+    return Math.floor((Math.random() * 12 + 1));
+}
+
+function purpleValue() {
+    return Math.floor((Math.random() * 12 + 1));
+}
+
+var randomChoice = numberToGuess(120, 19);
+var redGem = redValue();
+var greenGem = greenValue();
+var orangeGem = orangeValue(); 
+var purpleGem = purpleValue();
+
+function startGame() {
+    numberToGuess(120, 19);
+    redValue();
+    greenValue();
+    orangeValue();
+    purpleValue();
+}
+
+
+$("#numbertoguess-text").text(randomChoice);
+// $("#wins-text").text(otherNumber(120, 19));
+
+console.log(randomChoice);
+console.log(redGem);
+console.log(greenGem);
+console.log(orangeGem);
+console.log(purpleGem);
